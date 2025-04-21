872575d595fadd2a06ab5476943fe834ecc83a7c
akberofh <sd85bejal@code.edu.az>
2025-04-22 00:59:08 +0400

    sEFksdddesüsafssjjhfdej

--- routes/hesabatRouter.js
+++ routes/hesabatRouter.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import Salary from '../models/salaryModel.js';
 import { getMyTeamSalaries } from '../controllers/salaryController.js';
-import { adminControlAuth, userControlAuth } from '../middleware/authMiddleware.js';
+import {  userControlAuth } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 
-router.get('/maas', userControlAuth , adminControlAuth , async (req, res) => {
+router.get('/maas', async (req, res) => {
     try {
         const salaries = await Salary.find().sort({});
         res.json(salaries);
