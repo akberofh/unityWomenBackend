9eb28ddc0382d58fb05051ce0ac807d419053b7c
akberofh <sd85bejal@code.edu.az>
2025-04-22 04:06:57 +0400

    sEFksdddesüsafssjjhfdej

--- routes/qolbaqRoute.js
+++ routes/qolbaqRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import {upload, uploadToCloudinary } from '../middleware/uploadMiddleware.js'
 import { deleteById, getByCategoryQolbaq, getByIdQolbaq, getQolbaq, qolbaqAdd, qolbaqUpdate } from '../controllers/qolbaqController.js'
+import { adminControlAuth, userControlAuth } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
@@ -10,11 +11,11 @@ router.get('/', getQolbaq)
 router.get('/:catagory', getByCategoryQolbaq)
 
 
-router.post('/', upload.array('photo'),uploadToCloudinary,    qolbaqAdd)
+router.post('/', upload.array('photo'),uploadToCloudinary,  userControlAuth , adminControlAuth ,  qolbaqAdd)
 
 router.get('/id/:id', getByIdQolbaq)
 
-router.delete('/:id', deleteById)
+router.delete('/:id', userControlAuth, adminControlAuth, deleteById)
 
 router.put('/:id', upload.single('photo'), uploadToCloudinary,  qolbaqUpdate);
 
