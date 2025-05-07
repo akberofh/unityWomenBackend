3706e265be2562b10c1397552d17e7807490b571
akberofh <sd85bejal@code.edu.az>
2025-05-08 00:07:35 +0400

    sEFksdddesüsafssjjhfdej

--- models/confirmedCartModel.js
+++ models/confirmedCartModel.js
@@ -25,6 +25,10 @@ const confirmedCartSchema = new mongoose.Schema({
     }
   ],
   confirmedAt: { type: Date, default: Date.now },
+  gathered: {
+    type: Boolean,
+    default: false,
+  },
   paymentStatus: { type: String, enum: ['pending', 'paid', 'failed'], default: 'pending' }, // ödeme durumu
 });
 
