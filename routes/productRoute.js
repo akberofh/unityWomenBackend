5c5124166db8ac442765373100a3829345cdaf7e
akberofh <sd85bejal@code.edu.az>
2025-05-19 01:58:17 +0400

    sEFksdddesüsafssjjhfdej

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { userControlAuth } from "../middleware/authMiddleware.js";
 import {upload, uploadToCloudinary } from "../middleware/uploadMiddleware.js";
 import { addUserProduct, deleteUserProduct, getUserProduct,updateStock,confirmCart, updatePaymentStatus } from "../controllers/productController.js";
+import ConfirmedCart from '../models/confirmedCartModel.js';
 
 
 
@@ -14,6 +15,13 @@ router.put('/:productId', userControlAuth,updateStock);
 router.post('/confirm', userControlAuth, confirmCart);
 router.post('/payment', userControlAuth, updatePaymentStatus);
 
+router.get('/payment/:confirmedCartId', async (req, res) => {
+  const { confirmedCartId } = req.params;
+  const confirmedCart = await ConfirmedCart.findById(confirmedCartId);
+  if (!confirmedCart) return res.status(404).json({ error: "Cart not found" });
 
+  // confirmedCart.products sadece onaylanan ürünleri içermeli
+  res.json({ products: confirmedCart.products });
+});
 
 export default router;
