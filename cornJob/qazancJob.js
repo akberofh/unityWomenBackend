dd40b04b5078ff3220c44bc3df717ed60b76d01e
Sərvər Abdullayev <162313608+akberofh@users.noreply.github.com>
2025-06-19 03:18:15 +0400

    Update qazancJob.js

--- cornJob/qazancJob.js
+++ cornJob/qazancJob.js
@@ -1,23 +1,23 @@
-import cron from 'node-cron';
-import {  run } from '../config/qazanc.js';
+// import cron from 'node-cron';
+// import {  run } from '../config/qazanc.js';
 
-// Her 6 saatte bir Azerbaycan saatiyle çalışır: 00:00, 06:00, 12:00, 18:00
-const qazancJob = cron.schedule(
-  '0 */4 * * *',
-  async () => {
-    try {
-      console.log('✅ Maaş hesaplama başlatılıyor (AZT - Her 6 saatte bir)');
-      const result = await run();
-      console.log('💰 Maaşlar başarıyla hesaplandı:', result);
-    } catch (error) {
-      console.error('❌ Maaş hesaplama sırasında hata:', error.message);
-    }
-  },
-  {
-    timezone: 'Asia/Baku', 
-  }
-);
+// // Her 6 saatte bir Azerbaycan saatiyle çalışır: 00:00, 06:00, 12:00, 18:00
+// const qazancJob = cron.schedule(
+//   '0 */4 * * *',
+//   async () => {
+//     try {
+//       console.log('✅ Maaş hesaplama başlatılıyor (AZT - Her 6 saatte bir)');
+//       const result = await run();
+//       console.log('💰 Maaşlar başarıyla hesaplandı:', result);
+//     } catch (error) {
+//       console.error('❌ Maaş hesaplama sırasında hata:', error.message);
+//     }
+//   },
+//   {
+//     timezone: 'Asia/Baku', 
+//   }
+// );
 
-qazancJob.start();
+// qazancJob.start();
 
-export default qazancJob;
+// export default qazancJob;
